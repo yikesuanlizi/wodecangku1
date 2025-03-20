1b203ec2376ecf356012a0d49c2c0cfa4d820897
yikesuanlizi <2569217434@qq.com>
2025-03-21 01:08:47 +0800

    完成

--- src/App.vue
+++ src/App.vue
@@ -1,121 +1,85 @@
 <template>
   <div class="container">
-  <div class="header">
-  <div>
-    <input type="text" v-model="addTask" class="scanf"
-     @keyup.enter="addTodo" placeholder="请输入你的任务名称，按回车键确认">
+    <div class="header">
+      <div>
+        <input type="text" v-model="addTask" @keyup.enter="addTodo" placeholder="请输入任务...">
+      </div>
+      <div>
+        <task-row v-for="(todo, index) in todos" :key="todo.id" :todo="todo" :index="index" @delete-task="deleteTask" @edit-task="editTask" @update-completed="updateCompleted" />
+      </div>
+      <div class="footer">
+        <input type="checkbox" :checked="allChecked" @change="toggleAll">
+        <div>已完成 {{ completedCount }} / 全部 {{ todos.length }}</div>
+        <button @click="clearCompleted">清除已完成</button>
+      </div>
+    </div>
   </div>
-  <div>
-<taskRow 
-  v-for="(todo, index) in todos" 
-  :key="index" 
-  :todo="todo" 
-  :index="index"
-  @delete="deleteTodo(index)"
-  @edit="editTodo(index, $event)"
-  @update:completed="todo.completed = $event" 
-></taskRow>
-
-  </div>
-  <div class="footer">
-    <input type="checkbox" v-model="allChecked" @change="toggleAll">
-    <div>已完成 {{ completedCount }} / 全部 {{ todos.length }}</div>
-    <button @click="clearCompleted" style="background-color: red;border-radius: 4px;margin-right: 5px;">清除已完成任务</button>
-  </div>
-  </div>
-</div>
-
 </template>
 
 <script>
-import TaskRow from './components/TodoList.vue';
-import pubsub from 'pubsub-js';
+import pubsub from 'pubsub-js'
+import TaskRow from './components/TodoList.vue'
 
 export default {
-  name: 'App',
   components: {
-    TaskRow,
+    TaskRow
   },
   data() {
     return {
       addTask: '',
-      todos: JSON.parse(localStorage.getItem('todos')) || [],
-      allChecked: false,
-    };
+      todos: JSON.parse(localStorage.getItem('todos')) || []
+    }
   },
   computed: {
+    allChecked() {
+      return this.todos.every(todo => todo.completed);
+    },
     completedCount() {
       return this.todos.filter(todo => todo.completed).length;
-    },
+    }
   },
   methods: {
     addTodo() {
-      if (this.addTask.trim() === '') {
-        return this.todos.push({id: Date.now(), text: this})
-      }
-      const newTodo = { text: this.addTask, completed: false };
+      if (this.addTask.trim() === '') return;
+      const newTodo = {
+        id: Date.now(),
+        text: this.addTask,
+        completed: false
+      };
       this.todos.push(newTodo);
       localStorage.setItem('todos', JSON.stringify(this.todos));
       this.addTask = '';
     },
-    deleteTodo(index) {
+    editTask(index, task) {
+      this.todos[index].text = task;
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+    },
+    deleteTask(index) {
       this.todos.splice(index, 1);
       localStorage.setItem('todos', JSON.stringify(this.todos));
     },
-    editTodo(index, value) {
-      this.todos[index].text = value;
-      localStorage.setItem('todos', JSON.stringify(this.todos));
-    },
-   clearCompleted() {
-  this.todos = this.todos.filter(todo => !todo.completed);
-  // 添加深拷贝确保响应式更新
-  localStorage.setItem('todos', JSON.stringify([...this.todos])); 
-},
-
-    toggleAll() {
+    toggleAll(event) {
+      const checked = event.target.checked;
       this.todos.forEach(todo => {
-        todo.completed = this.allChecked;
+        todo.completed = checked;
       });
       localStorage.setItem('todos', JSON.stringify(this.todos));
     },
-  },
-  watch: {
-    todos: {
-      handler(newVal) {
-        this.allChecked = newVal.every(todo => todo.completed);
-      },
-      deep: true,
-    },
-  },
-  created() {
-    pubsub.subscribe('addTodo', (msg, data) => {
-      this.addTodo(data);
-    });
-    pubsub.subscribe('deleteTodo', (msg, data) => {
-      this.deleteTodo(data);
-    });
-    pubsub.subscribe('editTodo', (msg, data) => {
-      this.editTodo(data.index, data.value);
-    });
-  },
-  beforeDestroy() {
-    pubsub.unsubscribe('addTodo');
-    pubsub.unsubscribe('deleteTodo');
-    pubsub.unsubscribe('editTodo');
-  },
-};
+    clearCompleted() {
+      this.todos = this.todos.filter(todo => !todo.completed);
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+    }
+  }
+}
 </script>
 
 <style scoped>
-
 .container {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   height: 100vh; /* 让容器高度占满整个视口高度 */
-  border: 1px solid #000000;
-
 }
 .header {
   border: 1px solid #000000;
@@ -123,7 +87,6 @@ export default {
   padding-left: 10px;
   padding-right: 10px;
 }
-
 .scanf {
   width: 400px;
   height: 40px;
@@ -136,7 +99,6 @@ export default {
   border: 1px solid #000000;
   margin-bottom: 2px;
 }
-
 .footer {
   display: flex;
   justify-content: space-between;
@@ -145,9 +107,6 @@ export default {
   height: 40px;
   border: 1px solid #000000;
   margin-bottom: 3px;
-
 }
-
 /* 其他样式保持不变 */
 </style>
-
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,66 +1,57 @@
 <template>
   <div class="task-row">
     <div>
-      <input 
-        v-if="isEditing" 
-        type="text" 
-        v-model="todo.text" 
-        @blur="finishEdit"
-        @keydown.enter="finishEdit"
-        ref="editInput"
-      >
+      <input v-if="isEditing" type="text" v-model="editText" @blur="finishEdit" @keydown.enter="finishEdit" ref="editInput">
       <template v-else>
-        <input 
-          type="checkbox" 
-          v-model="todo.completed" 
-          @change="updateCompleted"
-        >
-        <span 
-          :class="{ completed: todo.completed }" 
-          @click="startEdit"
-        >
+        <input type="checkbox" v-model="todo.completed" @change="updateCompleted">
+        <span :class="{ completed: todo.completed }" @click="startEdit">
           {{ todo.text }}
         </span>
       </template>
     </div>
     <div>
-      <!-- 移除完成按钮 -->
       <button v-if="!isEditing" @click="editTodo" class="btn1">编辑</button>
-      <button @click="deleteTodo" class="btn2">删除</button>
+      <button @click="confirmDelete" class="btn2">删除</button>
     </div>
   </div>
 </template>
 
 <script>
-import pubsub from 'pubsub-js';
+import pubsub from 'pubsub-js'
 
 export default {
   name: 'TaskRow',
   props: {
     todo: Object,
-    index: Number,
+    index: Number
   },
   data() {
     return {
       isEditing: false,
-    };
+      editText: this.todo.text
+    }
+  },
+  mounted() {
+    // 订阅 addTask 事件
+    pubsub.subscribe('addTask', this.handleAddTask);
+    pubsub.subscribe('editTask', this.handleEditTask);
+    pubsub.subscribe('deleteTask', this.handleDeleteTask);
   },
   methods: {
-    updateCompleted() {
-      this.$emit('update:completed', this.todo.completed);// 触发父组件的更新事件
-      pubsub.publish('editTodo', { index: this.index, value: this.todo });// 触发全局的更新事件
-},
-    deleteTodo() {
-      const confirmDelete = confirm('确认删除吗？');
-      if (confirmDelete) {
-        pubsub.publish('deleteTodo', this.index);
+    handleAddTask(msg, addT) {
+      // 这里通常不需要在这个子组件里处理添加任务的逻辑
+      // 除非你有特别的需求希望子组件能够响应这个事件
+    },
+    handleEditTask(msg, payload) {
+      if (payload.index === this.index) {
+        this.editText = payload.task;
+        this.finishEdit();
       }
     },
-    editTodo() {
-      this.isEditing = true;
-      this.$nextTick(() => {
-        this.$refs.editInput.focus();
-      });
+    handleDeleteTask(msg, index) {
+      if (index === this.index) {
+        this.deleteTask();
+      }
     },
     startEdit() {
       this.isEditing = true;
@@ -69,15 +60,36 @@ export default {
       });
     },
     finishEdit() {
-      if (this.todo.text.trim() === '') {
-        alert('任务名称不能为空');
-        return;
-      }
       this.isEditing = false;
-      pubsub.publish('editTodo', { index: this.index, value: this.todo });
+      this.todo.text = this.editText;
+      // 直接在这里更新本地存储
+      localStorage.setItem('todos', JSON.stringify(this.$parent.todos));
     },
+    editTodo() {
+      this.startEdit();
+    },
+    updateCompleted() {
+      // 直接在这里更新本地存储
+      localStorage.setItem('todos', JSON.stringify(this.$parent.todos));
+    },
+    confirmDelete() {
+      if (confirm('确定要删除此任务吗？')) {
+        this.deleteTask();
+      }
+    },
+    deleteTask() {
+      // 直接在这里删除任务并更新本地存储
+      this.$parent.todos.splice(this.index, 1);
+      localStorage.setItem('todos', JSON.stringify(this.$parent.todos));
+    }
   },
-};
+  beforeDestroy() {
+    // 组件销毁前取消订阅
+    pubsub.unsubscribe('addTask', this.handleAddTask);
+    pubsub.unsubscribe('editTask', this.handleEditTask);
+    pubsub.unsubscribe('deleteTask', this.handleDeleteTask);
+  }
+}
 </script>
 
 <style scoped>
@@ -88,16 +100,13 @@ export default {
   width: 400px;
   border-bottom: 1px solid #ccc;
 }
-
 input[type="checkbox"] {
   accent-color: #00ffff;
 }
-
 .btn1 {
   background-color: #00ffff;
   border-radius: 4px;
 }
-
 .btn2 {
   background-color: red;
   border-radius: 4px;
